8bc8316168e4e4f845abaaa93f693a50fa39a86e
Eric Matthes <eric@Erics-MacBook-Pro.local>
2023-09-09 11:12:39 -0700

    Added files for MP 52, OOP 11, Inheritance.

---
+++ mp52_oop11_inheritance/account_2_free.py
@@ -0,0 +1,27 @@
+class Account:
+    """Model a user account on a site or app."""
+
+    def __init__(self, username, password=""):
+        self.username = username
+        self._store_password(password)
+
+    def welcome_user(self):
+        print(f"Welcome to the site, {self.username}!")
+
+    def close_account(self):
+        print(f"Closed account for {self.username}.")
+
+    def _store_password(self, password):
+        print(f"Stored password for {self.username}.")
+
+
+class FreeAccount(Account):
+
+    def welcome_user(self):
+        super().welcome_user()
+        print("You'll have limited access on a free account.")
+        
+
+if __name__ == "__main__":
+    account = FreeAccount("eric")
+    account.welcome_user()
