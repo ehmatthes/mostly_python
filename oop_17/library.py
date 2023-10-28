ebd2941ae33f8a33adc6a8801cf03327e97dbffe
Eric Matthes <ehmatthes@gmail.com>
2023-10-28 12:06:07 -0800

    Make code consistent between oop17 and oop18.

--- oop_17/library.py
+++ oop_17/library.py
@@ -22,18 +22,18 @@ class Library:
             print(f"- {info}")
 
 if __name__ == "__main__":
-    my_library = Library(name="The Climber's Library")
+    library = Library(name="The Climber's Library")
 
     book = Book(
         title="Freedom of the Hills",
         author="The Mountaineers"
     )
-    my_library.books.append(book)
+    library.books.append(book)
 
     book = Book(
         title = "Learning to Fly",
         author = "Steph Davis",
     )
-    my_library.books.append(book)
+    library.books.append(book)
 
-    my_library.show_books()
+    library.show_books()
