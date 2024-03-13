2710289833b2ba2bc241e2785e16e20b2fb6afce
Eric Matthes <ehmatthes@gmail.com>
2024-03-12 16:27:45 -0800

    Updated abs path program.

--- mp85_calc_file_paths/coffee_reader_2_absolute_path.py
+++ mp85_calc_file_paths/coffee_reader_2_absolute_path.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
-path = Path("/Users/eric/projects/coffee_project/coffees.txt")
+path = Path(
+    "/Users/eric/projects/coffee_project/coffees.txt")
 contents = path.read_text()
 
 print(contents)
