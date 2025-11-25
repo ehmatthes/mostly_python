37e36d867b62a5924f11ae214a872e6c633eb8c6
Eric Matthes <ehmatthes@gmail.com>
2025-11-25 16:48:04 -0500

    Code for MP 155, Debugging part 12.

---
+++ mp155_debugging_12/go_fish_bugfix/card_models.py
@@ -0,0 +1,105 @@
+import random
+from operator import attrgetter
+
+import card_data
+
+class Card:
+    
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    def __repr__(self):
+        symbol = card_data.suits_symbols[self.suit]
+        return f"{self.rank}{symbol}"
+
+    def __eq__(self, card_2):
+        """Two cards are equal if their ranks match."""
+        return self.rank == card_2.rank
+
+    def __gt__(self, card_2):
+        """A card is greater if its value is higher."""
+        return self.value > card_2.value
+
+    def __lt__(self, card_2):
+        """A card is lesser if its value is lesser."""
+        return self.value < card_2.value
+
+    @property
+    def value(self):
+        """Get a numerical value for the card."""
+        return card_data.ranks_values[self.rank]
+
+def same_suit(cards):
+    """Check if all cards have the same suit."""
+    suits = [c.suit for c in cards]
+    return len(set(suits)) == 1
+
+class Hand:
+
+    def __init__(self, cards=None):
+        if cards:
+            self.cards = cards
+        else:
+            self.cards = []
+
+    def organize(self):
+        """Put cards in a more usable order."""
+        self.cards.sort(key=attrgetter("value", "suit"))
+
+    def show(self, hidden=False):
+        if hidden:
+            hand_string = "  ".join(["X" for card in self.cards])
+        else:
+            hand_string = " ".join([str(card) for card in self.cards])
+        print(hand_string)
+
+class Deck:
+    """Bottom of the deck is the start of the list, top of the deck is the end
+    This makes drawing a card efficient.
+    """
+
+    def __init__(self):
+        self.cards = []
+        self.reset_deck()
+
+    def reset_deck(self):
+        """Build a standard, unshuffled deck."""
+        ranks = card_data.ranks_values.keys()
+        suits = card_data.suits_symbols.keys()
+
+        for rank in ranks:
+            for suit in suits:
+                card = Card(rank, suit)
+                self.cards.append(card)
+
+    def show(self):
+        deck_string = " ".join([str(card) for card in self.cards])
+        print(deck_string)
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def draw(self, num_cards=1):
+        """Draw one or more cards from top of the deck.
+        Return a single card, or a list of cards.
+        """
+        if num_cards == 1:
+            return self.cards.pop()
+
+        return [self.cards.pop() for _ in range(num_cards)]
+
+    def deal(self, num_hands=2, num_cards=5):
+        """Return a single Hand, or list of Hands."""
+        # Build empty hands.
+        hands = [Hand() for _ in range(num_hands)]
+
+        # Add cards to each hand, one at a time.
+        for _ in range(num_cards):
+            for hand in hands:
+                hand.cards.append(self.draw())
+
+        if len(hands) == 1:
+            return hands[0]
+
+        return hands
---
+++ mp155_debugging_12/go_fish_bugfix/go_fish.py
@@ -0,0 +1,75 @@
+"""Go Fish game, where one person plays against computer."""
+
+import sys
+import random
+
+from card_models import Card, Hand, Deck
+import go_fish_utils
+
+
+class GoFish:
+
+    def __init__(self):
+        # Start with a new shuffled deck.
+        random.seed(42)
+        self.deck = Deck()
+        self.deck.shuffle()
+
+    def start_game(self):
+        # Deal two hands.
+        hands = self.deck.deal(num_hands=2, num_cards=7)
+        self.player_hand, self.computer_hand = hands
+        
+        self.player_hand.organize()
+        self.computer_hand.organize()
+
+        self.player_pairs = []
+        self.computer_pairs = []
+
+        # Player goes first.
+        self.player_turn()
+
+    def player_turn(self):
+        """Manage the human player's turn."""
+        go_fish_utils.clear_terminal()
+        self.show_state()
+        requested_card = go_fish_utils.get_player_guess(
+            self.player_hand)
+        self.check_player_guess(requested_card)
+
+    def check_player_guess(self, guessed_rank):
+        """Process the player's guess."""
+        computer_ranks = [c.rank for c in self.computer_hand.cards]
+        if guessed_rank in computer_ranks:
+            # Correct guess. Remove card from both hands.
+            player_card = go_fish_utils.remove_card(
+                guessed_rank, self.player_hand)
+            computer_card = go_fish_utils.remove_card(
+                guessed_rank, self.computer_hand)
+
+            # Add cards to player's pairs.
+            self.player_pairs.append((player_card, computer_card))
+
+            # Player gets to go again.
+            msg = "\nYour guess was correct!"
+            msg += " Press Enter to continue."
+            input(msg)
+            self.player_turn()
+        else:
+            # It's the computer's turn now.
+            pass
+
+    def show_state(self):
+        """Show the current state of the game."""
+        print("Player hand:")
+        self.player_hand.show()
+
+        print("\nComputer hand:")
+        if "-v" in sys.argv:
+            self.computer_hand.show()
+        else:
+            self.computer_hand.show(hidden=True)
+
+if __name__ == "__main__":
+    gf_game = GoFish()
+    gf_game.start_game()
